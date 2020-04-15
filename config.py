f9694c06cf08d2bd8f1eaf6f8ca7c58f7abfbb60
xchange <xchange@live.com>
2020-04-16 00:37:29 +0800

    优化数据库配置

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ EVE_DB_DSN = os.getenv('EVE_DB_DSN', '')
 if not EVE_DB_DSN:
     print('未指定数据库地址, 程序终止')
     sys.exit(1)
-EVE_DB = create_engine(EVE_DB_DSN, pool_recycle=3600, pool_size=2)
+EVE_DB = create_engine(EVE_DB_DSN, pool_recycle=300, pool_size=1)
